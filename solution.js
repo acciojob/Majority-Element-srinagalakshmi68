bfdfdf2d0652de242fca3e83c95a8a6cb9ce0998
Srinagalakshmi <srinagalakshmi68@.com>
2022-07-12 21:21:28 +0530

    assignment added...

--- solution.js
+++ solution.js
@@ -1,11 +1,16 @@
 // arr = array of numbers
 function major(arr) {
     //your code here
-    let seen = {}
-
+    let map = {};
     for (let i = 0; i < arr.length; i++) {
-        seen[arr[i]] = (seen[arr[i]] || 0) + 1
-        if (seen[arr[i]] >= arr.length / 2) return arr[i]
+        //storing all the elemrnts of 'nums' Array in the 'map' object.
+        if (!map[arr[i]]) map[arr[i]] = 0;
+        map[arr[i]]++;
+    }
+    for (let key in map) {
+        //checking for value > nums.length/2
+        if (map[key] > arr.length / 2)
+            return key;
     }
 }
 module.exports = major;
