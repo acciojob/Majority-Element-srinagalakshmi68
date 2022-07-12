ecf28799fd6a623c75bb7dc32effa115c9200a56
Srinagalakshmi <srinagalakshmi68@.com>
2022-07-12 20:40:32 +0530

    assignment added

--- solution.js
+++ solution.js
@@ -1,6 +1,20 @@
 // arr = array of numbers
-function major(arr)
-{
-  //your code here
+function major(arr) {
+    //your code here
+    let map = new Map();
+
+    for (let i = 0; i < arr.length; i++) {
+        if (map.has(arr[i])) {
+            let count = map.get(arr[i]) + 1;
+            if (count > arr.length / 2) {
+                document.write("Majority found :- " + arr[i]);
+                return;
+            } else
+                map.set(arr[i], count);
+
+        } else
+            map.set(arr[i], 1);
+    }
+    document.write(" No Majority element");
 }
-module.exports=major;
+module.exports = major;
