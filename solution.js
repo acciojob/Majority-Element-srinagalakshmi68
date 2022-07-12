8e494d371f1eba03b183d2fa879432bca800d4f2
Srinagalakshmi <srinagalakshmi68@.com>
2022-07-12 21:02:31 +0530

    modified just now

--- solution.js
+++ solution.js
@@ -1,20 +1,14 @@
 // arr = array of numbers
 function major(arr) {
     //your code here
-    let map = new Map();
+    arr.sort((a, b) => a - b);
 
-    for (let i = 0; i < arr.length; i++) {
-        if (map.has(arr[i])) {
-            let count = map.get(arr[i]) + 1;
-            if (count > floor(arr.length / 2)) {
-                document.write("Majority found :- " + arr[i]);
-                return;
-            } else
-                map.set(arr[i], count);
+    const l = arr.length;
 
-        } else
-            map.set(arr[i], 1);
+    if (l % 2 === 0) {
+        return arr[l / 2]
+    } else {
+        return arr[(l - 1) / 2]
     }
-    document.write(" No Majority element");
 }
 module.exports = major;
